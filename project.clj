ab859d322690a5a095aa821035a19056870ff5a3
futuro <eniessenderry@gmail.com>
2015-08-17 21:46:13 -0500

    updated clojure dep to 1.7.0

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[com.stuartsierra/component "0.2.3"]
                  [honeysql "0.6.1"]
                  [org.apache.commons/commons-compress "1.9"]
-                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure "1.7.0"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/java.jdbc "0.4.1"]
